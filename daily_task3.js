f14e9806e0bfa7eb5c45581a4db473699ff2f743
rizagithub <113010774+rizagithub@users.noreply.github.com>
2023-03-17 14:43:09 +0700

    Add files via upload

---
+++ daily_task3.js
@@ -0,0 +1,362 @@
+let data=[
+    {
+      "_id": "64105749ca41ea104b00b7fb",
+      "age": 33,
+      "eyeColor": "green",
+      "name": "Stein Montoya",
+      "address": "Jakarta",
+      "registered": "2020-11-24T10:19:06 -07:00",
+      "friends": [
+        {
+          "id": 0,
+          "name": "Farid",
+          "isActive": false,
+          "favoriteFruit": "apple",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "football"
+            },
+            {
+              "id": 1,
+              "hobby": "baseball"
+            }
+          ]
+        },
+        {
+          "id": 1,
+          "name": "Theresia",
+          "isActive": false,
+          "favoriteFruit": "apple",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "football"
+            },
+            {
+              "id": 2,
+              "hobby": "basketball"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Theresia",
+          "isActive": true,
+          "favoriteFruit": "strawberry",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "baseball"
+            },
+            {
+              "id": 2,
+              "hobby": "basketball"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "_id": "6410574910af7edc19e426ac",
+      "age": 29,
+      "eyeColor": "green",
+      "name": "Ava Shepard",
+      "address": "Bandung",
+      "registered": "2022-01-07T03:23:30 -07:00",
+      "friends": [
+        {
+          "id": 0,
+          "name": "Shifly",
+          "isActive": false,
+          "favoriteFruit": "banana",
+          "gender": "female",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "basketball"
+            },
+            {
+              "id": 2,
+              "hobby": "basketball"
+            }
+          ]
+        },
+        {
+          "id": 1,
+          "name": "Theresia",
+          "isActive": false,
+          "favoriteFruit": "banana",
+          "gender": "female",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "baseball"
+            },
+            {
+              "id": 2,
+              "hobby": "football"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Shifly",
+          "isActive": false,
+          "favoriteFruit": "banana",
+          "gender": "female",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "baseball"
+            },
+            {
+              "id": 1,
+              "hobby": "football"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "_id": "6410574998da921d51f835eb",
+      "age": 29,
+      "eyeColor": "green",
+      "name": "Arline Schmidt",
+      "address": "Bali",
+      "registered": "2020-09-19T11:09:35 -07:00",
+      "friends": [
+        {
+          "id": 0,
+          "name": "Shifly",
+          "isActive": true,
+          "favoriteFruit": "apple",
+          "gender": "female",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "football"
+            },
+            {
+              "id": 1,
+              "hobby": "football"
+            }
+          ]
+        },
+        {
+          "id": 1,
+          "name": "Theresia",
+          "isActive": false,
+          "favoriteFruit": "strawberry",
+          "gender": "female",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "football"
+            },
+            {
+              "id": 1,
+              "hobby": "football"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Farid",
+          "isActive": false,
+          "favoriteFruit": "strawberry",
+          "gender": "female",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "football"
+            },
+            {
+              "id": 2,
+              "hobby": "baseball"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "_id": "64105749ece0da20d2a2f9c1",
+      "age": 34,
+      "eyeColor": "green",
+      "name": "Flynn Suarez",
+      "address": "Bali",
+      "registered": "2023-01-06T02:01:22 -07:00",
+      "friends": [
+        {
+          "id": 0,
+          "name": "Farid",
+          "isActive": true,
+          "favoriteFruit": "strawberry",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "baseball"
+            },
+            {
+              "id": 2,
+              "hobby": "basketball"
+            }
+          ]
+        },
+        {
+          "id": 1,
+          "name": "Theresia",
+          "isActive": true,
+          "favoriteFruit": "banana",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "basketball"
+            },
+            {
+              "id": 1,
+              "hobby": "football"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Theresia",
+          "isActive": false,
+          "favoriteFruit": "strawberry",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "basketball"
+            },
+            {
+              "id": 1,
+              "hobby": "basketball"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "_id": "64105749811f6839c44a726b",
+      "age": 34,
+      "eyeColor": "green",
+      "name": "Torres Parker",
+      "address": "Bandung",
+      "registered": "2020-02-21T03:12:20 -07:00",
+      "friends": [
+        {
+          "id": 0,
+          "name": "Farid",
+          "isActive": false,
+          "favoriteFruit": "strawberry",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "basketball"
+            },
+            {
+              "id": 1,
+              "hobby": "basketball"
+            }
+          ]
+        },
+        {
+          "id": 1,
+          "name": "Farid",
+          "isActive": true,
+          "favoriteFruit": "strawberry",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 1,
+              "hobby": "football"
+            },
+            {
+              "id": 1,
+              "hobby": "football"
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Farid",
+          "isActive": false,
+          "favoriteFruit": "strawberry",
+          "gender": "male",
+          "hobby": [
+            {
+              "id": 2,
+              "hobby": "baseball"
+            },
+            {
+              "id": 1,
+              "hobby": "baseball"
+            }
+          ]
+        }
+      ]
+    }
+]
+
+//1) display/print person yang registered dibawah tahun 2022
+function checkRegis(registered) {
+  return registered.substring(0, 4) < 2022
+}
+
+function loopingData (data) {
+  let arrPerson = []
+  for (let i = 0; i < data.length; i++) {
+      if(checkRegis(data[i].registered)) {
+          arrPerson.push (data[i])
+      }
+  }
+  return arrPerson
+}
+console.log(loopingData(data))
+
+// 2) display person yang address nya Bali
+function chekcAddress(address) { 
+  return address === "Bali" 
+}
+function checkCondition(key, value) {  
+  return key === value 
+} 
+function loopingData(data) {
+  let arr = [] 
+  for (let i = 0; i < data.length; i++) {
+      if (checkCondition("Bali", data[i].address)) {
+          arr.push(data[i])
+      }
+  }
+  return arr
+}
+console.log(loopingData(data))
+
+// 4) display hobby dari friends id 2
+console.log("====================================================")
+for(i=0; i<data.length; i++){
+  for(j=0; j<data[i].friends.length; j++){
+    for(k=0; k<data[i].friends[j].hobby.length; k++){
+      let hasil = data.map(function checkHobby(){
+        return data[i].friends[j]
+      })
+      console.log(hasil)
+    }
+  }
+}
+// console.log(hasil)
+
+function checkHobby(){
+  if(data[i].friends[j].hobby[k].hobby.id===2){
+    // console.log(data[i].friends[j])
+  }
+}
+
